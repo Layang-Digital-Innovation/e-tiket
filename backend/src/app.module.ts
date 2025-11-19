8542b1eb105b6afa7611381aa3741ac4a076d46c
anjasferdiansyah <anjasferdiansyah123@gmail.com>
2025-11-19 15:33:21 +0700

    fix app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -51,6 +51,8 @@ import { PayoutModule } from './payout/payout.module';
         redis: {
           host: configService.get('REDIS_HOST', 'localhost'),
           port: configService.get('REDIS_PORT', 6379),
+          password: configService.get('REDIS_PASSWORD'),
+          username: configService.get('REDIS_USERNAME'),
         },
       }),
     }),
